13930bc4de0b1693b718ae0aab18a785b7027567
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2026-03-10 23:21:01 +0000

    implimenter login logique utilisant un service de model (db)

---
+++ src/Controllers/login.js
@@ -0,0 +1,25 @@
+import findUserByMail from "../Models/database.js";
+const mail = document.querySelector("#mail");
+const password = document.querySelector("#password");
+const submit = document.querySelector("#submitbtn");
+
+submit.addEventListener("click", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const mailValue = mail.value;
+  const passwordValue = password.value;
+  // simuler la connexion à la base de données
+  setTimeout(() => {
+    const user = findUserByMail(mailValue, passwordValue);
+    if (user) {
+      // stocker l'objet user dans le sessionStorage
+      sessionStorage.setItem("user", JSON.stringify(user));
+
+      // rediriger vers la page dashboard
+      document.location = "dashboard.html";
+    } else {
+      alert("Invalid email or password");
+    }
+  });
+}
