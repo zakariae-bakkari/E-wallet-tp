1df59fe2e0eb18a469e0ba94d0b644770170ead6
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2026-03-17 18:19:15 +0000

    Update promise.js to fetch data from API and log response; adjust execution order of console logs

--- src/Controllers/promise.js
+++ src/Controllers/promise.js
@@ -7,5 +7,8 @@ Promise.resolve().then(() => {
   console.log("promise");
 });
 
-console.log("end");
+const data = fetch("https://jsonplaceholder.typicode.com/todos/4")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
+console.log("end");
